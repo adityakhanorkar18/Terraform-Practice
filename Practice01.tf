152e9ec9527d23268197e3a388f748ff271d02fb
Aditya Khanorkar <adityakhanorkar18.com>
2022-02-01 11:40:07 +0530

    Puspha rds code

--- Practice01.tf
+++ Practice01.tf
@@ -1,18 +1,11 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "Aditya"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
-  resource "aws_vpc" "MyVPC" {
-   cidr_block = "10.0.0.0/16"
-   tags = {
-     Name = "MyVPC1"
-   } 
-  }
-  resource "aws_instance" "Web" {
-    ami = "ami-005e54dee72cc1d00"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "Web1"
-    }
-  }
